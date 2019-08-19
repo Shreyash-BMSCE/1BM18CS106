3335d3c5c536b12099bb2c068843bfbdc4edef21
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-08-19 14:56:37 +0530

    Add files via upload

---
+++ Class/STACK_C1.C
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 20
+
+struct stack
+{
+	int a[MAX];
+	int top;
+}s1;
+
+void pop();
+void push();
+void disp();
+
+
+void main()
+{
+	char ch='y';
+	int ch1;
+	s1.top=-1;
+	while(ch=='y'|| ch=='Y')
+	{
+		printf("\n Enter Choice: \n 1:Push \t 2:PoP \n");
+		scanf("\n %d",&ch1);
+		switch(ch1)
+		{
+			case 1: push();
+				disp();
+				break;
+			case 2: pop();
+				disp();
+				break;
+			default: printf("\n Wrong input!!!");
+		}
+		printf("\n Do you want to continue?? y/n");
+		scanf("%s",&ch);
+	}
+	getch();
+}
+
+void push()
+{
+	if(s1.top==MAX-1)
+	{
+		printf("\n Overflow!!");
+	}
+	else
+	{
+		s1.a[++(s1.top)]=s1.top;
+	}
+}
+void pop()
+{
+	if(s1.top==-1)
+	{
+		printf("\n Underflow!!");
+	}
+	else
+	{
+		s1.top--;
+	}
+}
+void disp()
+{
+	int i;
+	i=s1.top;
+	while(i>=0)
+	{
+		printf("\n%d",s1.a[i]+1);
+		i--;
+	}
+	printf("<--");
+}
+
+
+
+
+
+
+
+
+
