e9ff4f7a7e5d1dd761137fad14fd90a98542a2dd
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-08-26 15:48:23 +0530

    Update QUEUE_1.C

--- Class/lab4_26.8.19/QUEUE_1.C
+++ Class/lab4_26.8.19/QUEUE_1.C
@@ -1,27 +1,78 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
 #define MAX 30
-//void queue(int,int *);
-//void dequeue(int,int *);
-//void disp(int);
+void enqueue(int *,int *);
+void dequeue(int *,int *, int *);
+void disp(int *, int *,int *);
 
 void main()
 {
-	int queue[MAX],val;
-	int r=0,f=-1;
+	int queue[MAX];
+	int r=-1,f=0;
 	int ch1;
 	char ch;
-	clrscr();
 	do
 	{
-		printf("\n Enter choice: \n 1.Insert 2.Delete 3.Display");
-		scanf("%d",ch1);
+		printf("\n Enter choice: \n 1.Enqueue 2.DeQueue 3.Display");
+		scanf("%d",&ch1);
 		switch(ch1)
 		{
-
-
-	getch();
+			case 1: enqueue(queue,&r);
+				break;
+			case 2: dequeue(queue,&r,&f);
+				break;
+			case 3: disp(queue,&r,&f);
+				break;
+			default: printf("\n Wrong Input!!!");
+		}
+		printf("\n Do you want to continue?? y/n:");
+		scanf("%s",&ch);
+	}while(ch=='y'||ch=='Y');
+}
+void enqueue(int a[],int *r)
+{
+		if(*r==MAX-1)
+		{
+			printf("\n Queue is empty!!");
+		}
+		else
+		{
+				int val;
+				printf("\n Enter the value:");
+				scanf("%d",&val);
+				*r++;
+				a[*r]=val;
+				printf("%d",a[*r]);
+		}
+}
+void dequeue(int a[],int *r,int *f)
+{
+		if(*f>*r)
+		{
+				printf("Reached End!! cannot Insert!!");
+		}
+		else
+		{
+				a[*f]=0;
+				*f++;
+		}
+}			
+void disp(int a[],int *r,int *f)
+{
+		if(*f>MAX-1)
+		{
+				printf("\n Queue Empty!!!");
+				//exit(0);
+		}
+		else
+		{
+				int i;
+				for(i=0;i<=*r;i++)
+				{
+						printf("%d",a[i]);
+				}
+		}
 }
 
-
+	
 
