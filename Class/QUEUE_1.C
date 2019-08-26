8a2c2be8f8d9e37798ba5cac85c6727f422f83cc
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-08-26 14:45:59 +0530

    Add files via upload

---
+++ Class/QUEUE_1.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 30
+//void queue(int,int *);
+//void dequeue(int,int *);
+//void disp(int);
+
+void main()
+{
+	int queue[MAX],val;
+	int r=0,f=-1;
+	int ch1;
+	char ch;
+	clrscr();
+	do
+	{
+		printf("\n Enter choice: \n 1.Insert 2.Delete 3.Display");
+		scanf("%d",ch1);
+		switch(ch1)
+		{
+
+
+	getch();
+}
+
+
+
