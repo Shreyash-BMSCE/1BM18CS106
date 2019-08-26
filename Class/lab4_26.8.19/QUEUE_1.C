d4c900c351da32ff75ebb4e3ec1fb5cafcc012a4
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-08-26 19:16:05 +0530

    Update QUEUE_1.C

--- Class/lab4_26.8.19/QUEUE_1.C
+++ Class/lab4_26.8.19/QUEUE_1.C
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 #define MAX 30
 void enqueue(int *,int *);
 void dequeue(int *,int *, int *);
@@ -11,9 +12,10 @@ void main()
 	int r=-1,f=0;
 	int ch1;
 	char ch;
+	clrscr();
 	do
 	{
-		printf("\n Enter choice: \n 1.Enqueue 2.DeQueue 3.Display");
+		printf("\n Enter choice: \n 1.Enqueue 2.DeQueue 3.Display \t");
 		scanf("%d",&ch1);
 		switch(ch1)
 		{
@@ -28,6 +30,7 @@ void main()
 		printf("\n Do you want to continue?? y/n:");
 		scanf("%s",&ch);
 	}while(ch=='y'||ch=='Y');
+	getch();
 }
 void enqueue(int a[],int *r)
 {
@@ -50,11 +53,12 @@ void dequeue(int a[],int *r,int *f)
 		if(*f>*r)
 		{
 				printf("Reached End!! cannot Insert!!");
+				exit(0);
 		}
 		else
 		{
 				a[*f]=0;
-				*f++;
+				(*f)++;
 		}
 }			
 void disp(int a[],int *r,int *f)
@@ -62,17 +66,14 @@ void disp(int a[],int *r,int *f)
 		if(*f>MAX-1)
 		{
 				printf("\n Queue Empty!!!");
-				//exit(0);
 		}
 		else
 		{
 				int i;
+				printf("-->");
 				for(i=*f;i<=*r;i++)
 				{
-						printf("%d",a[i]);
+						printf("\t %d",a[i]);
 				}
 		}
 }
-
-	
-
