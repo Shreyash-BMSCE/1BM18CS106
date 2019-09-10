923fd88432fb6d021f5a59c6573f2e7b636973e2
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-09-11 00:43:36 +0530

    Update str_cat.c

--- assignments/str_cat.c
+++ assignments/str_cat.c
@@ -3,16 +3,17 @@
 #include<stdlib.h>
 #include<conio.h>
 #define MAX 10
-//void input_s(char *);
-//void disp(char *);
+
 void main()
 {
 	struct strr
 	{
 		char a[MAX];
 	}s1[MAX],s2[MAX];
+	
 	int ch1,i,n1,n2,k;
 	char ch;
+	
 	clrscr();
 	do
 	{
@@ -20,58 +21,47 @@ void main()
 		scanf("%d",&ch1);
 		switch(ch1)
 		{
-			case 1:    /*
-			for(i=0;i<MAX;i++)
-			{
-				s1[i].a[i]=' ';
-				s2[i].a[i]=' ';
-			}   */
+			case 1: printf("\n Enter the number of inputs for list 1:");
+				scanf("%d",&n1);
 
-			printf("\n Enter the number of inputs for list 1:");
-			scanf("%d",&n1);
-
-		    for(i=0;i<n1;i++)
-		    {
-			printf("\n Enter the number of letters you want to input:");
-			scanf("%d",&k);
-			fflush(stdin);
-			printf("\n Enter the string:");
-			scanf("%s",&s1[i].a);
-			s1[i].a[k]='\0';
-		    }
-		    //s1[0].a[1]='\0';
-		    printf("\n Enter the number of inputs for list 2:");
-		    scanf("%d",&n2);
-		    for(i=0;i<n2;i++)
-		    {
-			printf("\n Enter the string :");
-			fflush(stdin);
-			scanf("%s",&s2[i].a);
-			//k=strlen(s2[i].a);
-			//s2[i].a[k+1]='\0';
-		    }
-		    break;
-			case 2:
-			--n2;
-			for(i=0;i<=(n2+1);i++) {
-
-			if(s1[i].a=="None")
-			{
-				strcpy(s1[i].a," ");
-				strcpy(s1[i].a,s2[i].a);
-			}
-			else if(s2[i].a=="None")
-			{
-				strcat(s1[i].a," ");
-			}
-			else
-			strcat(s1[i].a,s2[n2-i].a);
-			k = strlen(s1[i].a);
-			s1[i].a[k+1]='\0';
-			fflush(stdout);
-			printf(" %s ",s1[i].a);
-			}
-			break;
+		    		for(i=0;i<n1;i++)
+		   		 {
+					printf("\n Enter the number of letters you want to input:");
+					scanf("%d",&k);
+					fflush(stdin);
+					printf("\n Enter the string:");
+					scanf("%s",&s1[i].a);
+					s1[i].a[k]='\0';
+		 		 }
+		    		printf("\n Enter the number of inputs for list 2:");
+		    		scanf("%d",&n2);
+		    		for(i=0;i<n2;i++)
+		   		{
+					printf("\n Enter the string :");
+					fflush(stdin);
+					scanf("%s",&s2[i].a);
+		   	 	}
+		    		break;
+			case 2:--n2;
+				for(i=0;i<=(n2+1);i++)
+				{
+					if(s1[i].a=="None")
+					{
+						strcpy(s1[i].a," ");
+						strcpy(s1[i].a,s2[i].a);
+					}
+					else if(s2[i].a=="None")
+					{
+						strcat(s1[i].a," ");
+					}
+					else
+					strcat(s1[i].a,s2[n2-i].a);
+					k = strlen(s1[i].a);
+					s1[i].a[k+1]='\0';
+					fflush(stdout);
+					printf(" %s ",s1[i].a);
+				}
+				break;
 			default: printf("\n Wrong Input!!!");
 		}
 		printf("\n Do you want to continue?? y/n:");
