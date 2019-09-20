42d45658f376dde08917b27f5640d59edd1953aa
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-09-20 23:59:00 +0530

    Create TowerOfHanoi.c

---
+++ OtherProbs/TowerOfHanoi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void towerOfHanoi(int n, char src,
+					char dest, char temp)
+{
+	if (n == 1)
+	{
+		printf("Move disk 1 from rod %c to rod %c \n",src ,dest);
+		return;
+	}
+	towerOfHanoi(n - 1, src, temp, dest);
+	printf("Move disk %d from rod %c to rod %c \n",n ,src ,dest);
+	//cout << "Move disk " << n << " from rod " << src <<
+	//							" to rod " << dest << endl;
+	towerOfHanoi(n - 1, temp, dest, src);
+}
+
+// Driver code
+void main()
+{
+	int n = 4; // Number of disks
+	//printf("\n Enter n:");
+	//scanf("%d",&n);
+	towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+}
