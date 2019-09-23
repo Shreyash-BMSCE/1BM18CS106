46da94b025c91ea5a5e2402311819769b0426fa6
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2019-09-23 21:32:07 +0530

    Update linklist.c

--- Class/23.9.19/linklist.c
+++ Class/23.9.19/linklist.c
@@ -6,68 +6,48 @@ struct LinkList
     int data;
     struct LinkList *link;
 };
-typedef struct LinkList *NODE;
-NODE create_node();
-NODE insert_beg(NODE);
-NODE del_beg(NODE);
-void disp_node(NODE);
 
-void main()
-{   
-    int i,ch=0;
-    NODE first;
-    first = NULL;
-    while(ch!=4)
-    {
-        printf("\n Enter Choice: \n 1. Insert Node  2. Delete from Beginning  3. Display  4. Exit \t");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1: first = insert_beg(first);
-                    break;
-            case 2: first = del_beg(first);
-                    break;
-            case 3: disp_node(first);
-                    break;
-            default: printf("\n Wrong Input!!!");  
-        }
-    }
-}
+typedef struct LinkList *NODE;
 
 NODE create_node()
 {
-    NODE node;
-    node = (NODE) malloc(sizeof(NODE));
-    node->link = NULL;
-    return node;
+    NODE temp;
+    temp = (NODE)malloc(sizeof(NODE));
+    if(temp==NULL)
+    {
+        printf("\n No Memory!!");
+        exit(0);
+    }
+    return temp;
 }
 
-NODE insert_beg(NODE first)
+NODE insert_beg(NODE first,int val)
 {
-    int val;
-    //NODE tempf;
-    NODE temp = create_node();
-    printf("\n Enter data: ");
-    scanf("%d",&val);
+    NODE temp;
+    temp = create_node();
     temp -> data = val;
-
-    if(first==NULL)
-    {
-        printf("\n FIRST ELEMENT!!! \n");
-        first = temp;
-    }    
-    else
-    {
-        //tempf = first;
-        temp->link = first;
-        first = temp;
-    }
+    temp->link = first;
+    first = temp;
     return first;
 }
-
-NODE del_beg(NODE first)
+NODE insert_end(NODE first,int item)
 {
-    if(first==NULL)
+  NODE temp,x;
+  x = create_node();
+  x->data=item;
+  x->link=NULL;
+  if(first==NULL)
+     return x;
+  temp=first;
+  while(temp->link!=NULL)
+     temp=temp->link;
+  temp->link=x;
+  return first;
+}
+
+/*NODE del_beg(NODE first)
+{
+    if(first == NULL)
     {
         printf("\n LIST EMPTY!!!");
     }
@@ -82,22 +62,60 @@ NODE del_beg(NODE first)
         free(temp);
     }
     return first;
-}
+} */
 
-void disp_node(NODE first)
+void display(NODE first)
 {
+    NODE temp;
     if(first==NULL)
     {
-        printf("\n LIST EMPTY!!!");
+        printf("\n LIST EMPTY!!!\n");
     }
     else
     {
-        NODE temp;
-        temp = first;
-        while(temp->link!=NULL);
+        temp=first;
+        printf("Elements are: ");
+        while(temp!=NULL)
         {
-            printf("%d -> ",temp->data);
-            temp = temp->link;
+            printf("%d ",temp->data);
+            temp=temp->link;
+        }
+        printf("\n");
+    }
+} 
+
+
+/*NODE create_node();
+NODE insert_beg(NODE);
+NODE del_beg(NODE);
+void disp_node(NODE);
+*/
+int main()
+{   
+    NODE first = NULL;
+    int ch,val;
+    while(1)
+    {
+        printf("\n Enter Choice: \n 1. Insert Node at front  2. Insert Node at end  3. Display  4. Exit \t");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("\n Enter data: ");
+                    scanf("%d",&val);
+                    first = insert_beg(first,val);
+                    break;
+            /*case 2: first = del_beg(first);
+                    break;  */
+            case 2: printf("Enter the element to be inserted: ");
+                    scanf("%d",&val);
+                    first=insert_end(first,val);
+                    break;
+            case 3: display(first);
+                    break;
+            case 4: exit(0);
+            default: printf("\n Wrong Input!!!");  
         }
     }
+    return 0;
 }
+
